aa87999b9e193f95613f48d51e62efb88af5a1db
Mitsuru Nakakawaji <mitsuru@procube.jp>
2023-04-16 09:28:51 +0900

    [[PRERELEASE]] update mosquitto and conusl

--- SOURCES/consul-env2conf.sh
+++ SOURCES/consul-env2conf.sh
@@ -8,7 +8,7 @@ tr "\000" "\n" < /proc/1/environ > /var/run/SYSTEMD_ENV
 BOOTSTRAP_EXPECT=1
 RETRY_JOIN=
 if [ -n "$PEERS" ]; then
-  RETRY_JOIN="\"retry_join\": [$PEERS],"
+  RETRY_JOIN="retry_join = [$(echo $PEERS |  sed 's/\([^,]*\)/"\1"/g')]"
   OIFS=$IFS;IFS=","
   set -- $PEERS
   BOOTSTRAP_EXPECT=$#
@@ -17,31 +17,37 @@ fi
 
 ENCRYPT=
 if [ -n "$CONSUL_SHARED_KEY" ]; then
-  ENCRYPT="\"encrypt\": [$CONSUL_SHARED_KEY],"
+  ENCRYPT="encrypt = \"$CONSUL_SHARED_KEY\""
 fi
 
 NODE_NAME=
 if [ -n "$CONSUL_NODE_NAME" ]; then
-  NODE_NAME="\"node_name\": \"$CONSUL_NODE_NAME\","
+  NODE_NAME="node_name = \"$CONSUL_NODE_NAME\""
 fi
 
 ACL=
 if [ -n "$CONSUL_ACL_ENABLED" ]; then
-  ACL=',"acl" : { "enabled": true, "default_policy": "deny", "enable_token_persistence": true}'
+  ACL='acl = {
+  enabled = true
+  default_policy = "deny"
+  enable_token_persistence = true
+}'
 fi
-cat > /etc/consul.json << __EOF
-{
-  "data_dir" : "/var/consul",
-  "log_level": "INFO",
-  "bootstrap_expect": ${BOOTSTRAP_EXPECT},
-  "bind_addr": "0.0.0.0",
-  "advertise_addr": "{{ GetPrivateIP }}",
-  ${RETRY_JOIN}
-  ${ENCRYPT}
-  ${NODE_NAME}
-  "client_addr": "0.0.0.0",
-  "server": true,
-  "disable_host_node_id": true
-  ${ACL}
+cat > /etc/consul.d/consul.hcl << __EOF
+data_dir = "/var/consul"
+log_level = "INFO"
+bootstrap_expect = ${BOOTSTRAP_EXPECT}
+bind_addr = "0.0.0.0"
+advertise_addr = "{{ GetPrivateIP }}"
+${RETRY_JOIN}
+${ENCRYPT}
+${NODE_NAME}
+client_addr = "0.0.0.0"
+server = true
+limits ={
+  http_max_conns_per_client = 4096
 }
+disable_host_node_id = true
+${ACL}
+
 __EOF
